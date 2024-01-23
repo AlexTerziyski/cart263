f30ac2d2740d159e5a8abb7ecb7ca7e44ef5d026
AlexTerziyski <alexander.terziyski@mail.concordia.ca>
2024-01-23 13:19:15 -0500

    A1: wheres-sausage-dog

---
+++ activities/wheres-sausage-dog/js/script.js
@@ -0,0 +1,31 @@
+/**
+Activity: wheres-sausage-dog
+Alex Terziyski
+
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
