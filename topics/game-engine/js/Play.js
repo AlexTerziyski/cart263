b9eb81d3ba258bf71d665c392edd1b89707586a0
AlexTerziyski <alexander.terziyski@mail.concordia.ca>
2024-03-19 06:04:23 -0400

    Game-Engine: Edits

--- topics/game-engine/js/Play.js
+++ topics/game-engine/js/Play.js
@@ -1,4 +1,15 @@
-
+/**
+ * Game-Engine Jam
+ * Alex Terziyski
+ * 
+ * Prototype for testing out physics some of Phaser 3 set templates, relying on mathematical calculations instead.
+ * 
+ * This prototype is a bow and arrow game that explores the possibility of implementing game mechanics and interactions using mathematical formulas instead of built-in physics engines.
+ * Players control a bow to shoot arrows at a target bag, aiming to score points. The game features simple physics simulations for arrow trajectory and collision detection.
+ * My goal was to experiment with physics interactions without necessarily using Phaser 3's gravity, velocity, physics (just to see if its possible and challenge myself). 
+ * 
+ * Click/Press your mouse to shoot the arrow at the bag to score points!
+ */
 
 class Play extends Phaser.Scene {
     constructor() {
